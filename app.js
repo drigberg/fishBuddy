d65e9773e5da02eef6914097d5c0fad1fdbbe438
Daniel Rigberg <daniel.rigberg@gmail.com>
2017-01-25 12:09:31 -0500

    add basic back-end for serving images

---
+++ app.js
@@ -0,0 +1,24 @@
+var express    	= require("express"),
+    app        	= express(),
+    bodyParser  = require("body-parser"),
+    port       	= process.env.PORT || 5000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+// seedDB();
+
+//ROOT ROUTE
+app.get("/", function (req, res){
+    res.render("fishBuddy");
+});
+
+//safety net redirect
+app.get("*", function (req, res){
+    res.redirect("/");
+});
+
+app.listen(port, function(err){
+    console.log("Personal site server is running on port " + port);
+});
